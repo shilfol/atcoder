b00c150780f625484b20306545a97b18b1801a87
shilfol <yayananoda@gmail.com>
2018-03-26 13:35:17 +0900

    Q0のコミット忘れ

---
+++ abs/beginner.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////
+///        templates                 ///
+////////////////////////////////////////
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readBigLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func strSprit(str string) []string {
+	cols := strings.Split(str, " ")
+	return cols
+}
+
+func parseInt(str string) int {
+	n, _ := strconv.Atoi(str)
+	return n
+}
+
+type SortSlice []int
+
+func (s SortSlice) Len() int {
+	return len(s)
+}
+
+func (s SortSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortSlice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func reverseString(str string) string {
+	buf := []rune(str)
+	for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
+		buf[i], buf[j] = buf[j], buf[i]
+	}
+	return string(buf)
+}
+
+////////////////////////////////////////
+///        end templates             ///
+////////////////////////////////////////
+
+func main() {
+	line := nextLine()
+	a := parseInt(line)
+
+	line = nextLine()
+	spl := strSprit(line)
+	b := parseInt(spl[0])
+	c := parseInt(spl[1])
+
+	line = nextLine()
+
+	fmt.Println(a+b+c, line)
+}
