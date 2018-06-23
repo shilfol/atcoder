ea8748aed4f8423589ee24ead278a9cf361c9b49
shilfol <yayananoda@gmail.com>
2018-06-23 23:54:07 +0900

    101 a,b (c,d)

---
+++ abc/101/d.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////
+///        templates                 ///
+////////////////////////////////////////
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readBigLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func strSprit(str string) []string {
+	cols := strings.Split(str, " ")
+	return cols
+}
+
+func parseInt(str string) int {
+	n, _ := strconv.Atoi(str)
+	return n
+}
+
+func intSprit(str string) []int {
+	strs := strSprit(str)
+	cols := make([]int, len(strs))
+	for i, v := range strs {
+		cols[i] = parseInt(v)
+	}
+	return cols
+}
+
+type SortSlice []int
+
+func (s SortSlice) Len() int {
+	return len(s)
+}
+
+func (s SortSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s SortSlice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func reverseString(str string) string {
+	buf := []rune(str)
+	for i, j := 0, len(buf)-1; i < j; i, j = i+1, j-1 {
+		buf[i], buf[j] = buf[j], buf[i]
+	}
+	return string(buf)
+}
+
+////////////////////////////////////////
+///        end templates             ///
+////////////////////////////////////////
+
+func main() {
+	line := readBigLine()
+	K := parseInt(line)
+
+	keta := -1
+
+	for i := 1; i <= K; i++ {
+		n := i / 10
+		if n == 0 {
+			fmt.Println(i)
+			continue
+		}
+		t := (i-1)%9 + 1
+		if t == 1 {
+			keta++
+		}
+
+		var ret int64
+		for j := 0; j <= keta; j++ {
+			ret += 9 * int64(math.Pow10(j))
+		}
+
+		fmt.Println(int64(math.Pow10(keta+1))*int64(t) + ret)
+
+	}
+}
