205005c83fed93958698368cafbef48154ff7fca
shilfol <yayananoda@gmail.com>
2018-09-29 22:38:50 +0900

    abc111 a,b,c

---
+++ abc/111/a.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////
+///        templates                 ///
+////////////////////////////////////////
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readBigLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func strSprit(str string) []string {
+	cols := strings.Split(str, " ")
+	return cols
+}
+
+func parseInt(str string) int {
+	n, _ := strconv.Atoi(str)
+	return n
+}
+
+func intSprit(str string) []int {
+	strs := strSprit(str)
+	cols := make([]int, len(strs))
+	for i, v := range strs {
+		cols[i] = parseInt(v)
+	}
+	return cols
+}
+
+////////////////////////////////////////
+///        end templates             ///
+////////////////////////////////////////
+
+func main() {
+	line := nextLine()
+
+	for i := 0; i < 3; i++ {
+
+		if string(line[i]) == "1" {
+			fmt.Print(9)
+		} else {
+			fmt.Print(1)
+		}
+	}
+	fmt.Println()
+}
---
+++ abc/111/b.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////
+///        templates                 ///
+////////////////////////////////////////
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readBigLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func strSprit(str string) []string {
+	cols := strings.Split(str, " ")
+	return cols
+}
+
+func parseInt(str string) int {
+	n, _ := strconv.Atoi(str)
+	return n
+}
+
+func intSprit(str string) []int {
+	strs := strSprit(str)
+	cols := make([]int, len(strs))
+	for i, v := range strs {
+		cols[i] = parseInt(v)
+	}
+	return cols
+}
+
+////////////////////////////////////////
+///        end templates             ///
+////////////////////////////////////////
+
+func main() {
+	line := nextLine()
+
+	n := parseInt(line)
+
+	var t, b int
+	t, n, b = n/100, n%100/10, n%10
+
+	if t != n || t != b {
+		if t < n || t < b {
+			t++
+		}
+	}
+
+	fmt.Println(t*100 + t*10 + t)
+}
