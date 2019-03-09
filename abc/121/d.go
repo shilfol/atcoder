13d0986468a7701ee3e48eac927c9aa9b04081ad
shilfol <yayananoda@gmail.com>
2019-03-09 22:32:50 +0900

    abc 121 abc, d

---
+++ abc/121/d.go
@@ -0,0 +1,123 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+////////////////////////////////////////
+///        templates                 ///
+////////////////////////////////////////
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+var rdr = bufio.NewReaderSize(os.Stdin, 1000000)
+
+func readBigLine() string {
+	buf := make([]byte, 0, 1000000)
+	for {
+		l, p, e := rdr.ReadLine()
+		if e != nil {
+			panic(e)
+		}
+		buf = append(buf, l...)
+		if !p {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func strSprit(str string) []string {
+	cols := strings.Split(str, " ")
+	return cols
+}
+
+func parseInt(str string) int {
+	n, _ := strconv.Atoi(str)
+	return n
+}
+
+func intSprit(str string) []int {
+	strs := strSprit(str)
+	cols := make([]int, len(strs))
+	for i, v := range strs {
+		cols[i] = parseInt(v)
+	}
+	return cols
+}
+
+func bitCount(n uint) int {
+
+	x := uint64(n)
+
+	const m = 1<<64 - 1
+	const m0 = 0x5555555555555555
+	const m1 = 0x3333333333333333
+	const m2 = 0x0f0f0f0f0f0f0f0f
+
+	x = x>>1&(m0&m) + x&(m0&m)
+	x = x>>2&(m1&m) + x&(m1&m)
+	x = (x>>4 + x) & (m2 & m)
+	x += x >> 8
+	x += x >> 16
+	x += x >> 32
+
+	return int(x) & (1<<7 - 1)
+}
+
+func bitExist(n, i int) bool {
+	return ((n >> uint(i)) & 1) == 1
+}
+
+func setBit(d, n int) int {
+	t := 1 << uint(n)
+	return d | t
+}
+
+func intAbs(n int) int {
+	return int(math.Abs(float64(n)))
+}
+
+////////////////////////////////////////
+///        end templates             ///
+////////////////////////////////////////
+func xors(n int64) int64 {
+	ret := int64(0)
+	if n%2 == 0 {
+		if (n/2)%2 == 0 {
+			ret = int64(0) ^ n
+		} else {
+			ret = int64(1) ^ n
+		}
+	} else {
+		if ((n+1)/2)%2 == 0 {
+			ret = int64(0)
+		} else {
+			ret = int64(1)
+		}
+	}
+	return ret
+}
+
+func main() {
+	line := nextLine()
+	spl := strSprit(line)
+
+	A, _ := strconv.ParseInt(spl[0], 10, 64)
+	B, _ := strconv.ParseInt(spl[1], 10, 64)
+
+	xa := xors(A - 1)
+	xb := xors(B)
+
+	fmt.Println(xa ^ xb)
+}
